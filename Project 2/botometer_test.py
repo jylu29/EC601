694f4974cb0089e1b86199096db13c0c5a4efb7b
jylu <1@1>
2022-10-17 21:11:17 -0400

    Botometer test code

---
+++ Project 2/botometer_test.py
@@ -0,0 +1,20 @@
+import botometer
+
+
+def initialize_boto():
+    rapidapi_key = "rapidapi_key"
+    twitter_app_auth = {
+        'consumer_key': 'consumer_key',
+        'consumer_secret': 'consumer_secret',
+        'access_token': 'access_token',
+        'access_token_secret': 'access_token_secret',
+    }
+    return botometer.Botometer(wait_on_ratelimit=True,
+                               rapidapi_key=rapidapi_key,
+                               **twitter_app_auth)
+
+
+if __name__ == '__main__':
+    bom = initialize_boto()
+    bom.check_accounts_in(['@clayadavis', '@onurvarol', '@jabawack'])
+    print(bom.check_account('@clayadavis'))
